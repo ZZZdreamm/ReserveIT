6e67f1057cf1e9a44170de01a4676275d28caf5e
ZZZdream <kmultan1234@gmail.com>
2023-11-10 19:14:03 +0100

    Add React Portal to body

---
+++ frontend/src/common/Portal.tsx
@@ -0,0 +1,9 @@
+"use client"
+import ReactDOM from "react-dom";
+
+export default function Portal({ children }: any) {
+  if (typeof document == "undefined") {
+    return <>{children}</>;
+  }
+  return ReactDOM.createPortal(children, document.body);
+}
