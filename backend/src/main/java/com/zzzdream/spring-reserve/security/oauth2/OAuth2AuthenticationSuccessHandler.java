ead7c7294a04bd68d973020eb484253fcc12f738
ZZZdream <kmultan1234@gmail.com>
2023-11-10 18:59:22 +0100

    Remove old comment

--- backend/src/main/java/com/zzzdream/spring-reserve/security/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ backend/src/main/java/com/zzzdream/spring-reserve/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -78,7 +78,6 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         return appProperties.getOauth2().getAuthorizedRedirectUris()
                 .stream()
                 .anyMatch(authorizedRedirectUri -> {
-                    // Only validate host and port. Let the clients use different paths if they want to
                     URI authorizedURI = URI.create(authorizedRedirectUri);
                     if(authorizedURI.getHost().equalsIgnoreCase(clientRedirectUri.getHost())
                             && authorizedURI.getPort() == clientRedirectUri.getPort()) {
