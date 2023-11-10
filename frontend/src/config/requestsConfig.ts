6f683ed738080b625afcb71f8135f8fa70fa3bf1
ZZZdream <kmultan1234@gmail.com>
2023-11-10 19:09:34 +0100

    Add constants and global requests configuration

---
+++ frontend/src/config/requestsConfig.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosHeaders } from "axios";
+import { ACCESS_TOKEN } from "./constants";
+// require('dotenv').config(); // Load environment variables from a .env file
+
+let apiUrl = "https://api.github.com";
+
+const localUrl = "http://localhost:8080/";
+
+if (process.env.NODE_ENV === "development") {
+  apiUrl = localUrl;
+}
+
+const login = process.env.NEXT_PUBLIC_API_USERNAME;
+const password = process.env.NEXT_PUBLIC_API_PASSWORD;
+
+const headers = new AxiosHeaders({
+  "Content-Type": "application/json",
+});
+
+export const axiosBase = axios.create({
+  baseURL: apiUrl,
+  headers: headers,
+});
